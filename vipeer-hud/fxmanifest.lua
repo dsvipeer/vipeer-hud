73a26a6d0f4e8dd2c9c2277339651595dd787d0d
dsvipeer <112516086+dsvipeer@users.noreply.github.com>
2023-10-03 08:41:20 -0300

    Add files via upload

---
+++ vipeer-hud/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'https://github.com/dsvipeer'
+description 'A Better HUD that shows location, time, fuel and have a fluid animation to the speedometer when entering and exiting a car.'
+version '1.0'
+
+client_scripts { "client.lua" }
+
+ui_page "ui/index.html"
+files { "ui/**/*" }                            
